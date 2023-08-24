bc3cd0242f5a64617be9d8364a6907755feb3688
Mustafa Gezen <mustafa@rockylinux.org>
2023-08-24 09:25:11 +0200

    addlicense

--- devtools/nfv2/main.go
+++ devtools/nfv2/main.go
@@ -1,3 +1,17 @@
+// Copyright 2023 Peridot Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package main implements nfv2 (nofussvendor 2) which is a tool to make sure the generated
 // artifacts from Bazel can be used with native Go tooling. This is done by generating
 // a go.mod file in bazel-bin to match the package name and then add a replace directive
