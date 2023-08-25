fbf14f6c113284b6ea4a85d5ae70ae1e4fd8f5c2
Mustafa Gezen <mustafa@rockylinux.org>
2023-08-25 18:44:24 +0200

    Add SqlNullString PB helper

--- base/go/wrapper_helpers.go
+++ base/go/wrapper_helpers.go
@@ -17,6 +17,7 @@ package base
 import (
 	"database/sql"
 	"google.golang.org/protobuf/types/known/timestamppb"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 )
 
 func SqlNullTime(t sql.NullTime) *timestamppb.Timestamp {
@@ -24,4 +25,11 @@ func SqlNullTime(t sql.NullTime) *timestamppb.Timestamp {
 		return nil
 	}
 	return timestamppb.New(t.Time)
+}
+
+func SqlNullString(s sql.NullString) *wrapperspb.StringValue {
+	if !s.Valid {
+		return nil
+	}
+	return wrapperspb.String(s.String)
 }
