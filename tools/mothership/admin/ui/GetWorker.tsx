821ad656d9270e834d31ee2cbb8273c5f30b0c9a
Mustafa Gezen <mustafa@rockylinux.org>
2023-08-26 18:36:15 +0200

    mothership/admin/ui: Add delete worker feature

--- tools/mothership/admin/ui/GetWorker.tsx
+++ tools/mothership/admin/ui/GetWorker.tsx
@@ -15,7 +15,7 @@
  */
 
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import { ResourceView } from 'base/ts/mui/ResourceView';
 import { reqap } from 'base/ts/reqap';
@@ -25,12 +25,20 @@ import { mshipAdminApi } from 'tools/mothership/admin/ui/api';
 import { V1Worker } from 'bazel-bin/tools/mothership/proto/admin/v1/mshipadminpb_ts_proto_gen';
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogContent from '@mui/material/DialogContent';
 
 export const GetWorker = () => {
+  const navigate = useNavigate();
+
   const params = useParams();
   const [resource, setResource] = React.useState<V1Worker | undefined | null>(
     undefined,
   );
+  const [deleteOpen, setDeleteOpen] = React.useState<boolean>(false);
 
   // Load the resource
   React.useEffect(() => {
@@ -50,9 +58,52 @@ export const GetWorker = () => {
     })().then();
   }, []);
 
+  const doDelete = async () => {
+    const [res, err] = await reqap(
+      mshipAdminApi.deleteWorker({
+        name: `workers/${params.name}`,
+      }),
+    );
+
+    if (err) {
+      setDeleteOpen(false);
+      return;
+    }
+
+    navigate('/');
+  };
+
   return (
     <Box>
-      <Box sx={{ p: 1.5 }}>workers/{params.name}</Box>
+      <Dialog open={deleteOpen}>
+        <DialogContent>
+          <DialogContentText>
+            Are you sure you want to delete this worker?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setDeleteOpen(false)}>Cancel</Button>
+          <Button onClick={doDelete} color="error">Delete</Button>
+        </DialogActions>
+      </Dialog>
+      <Box
+        sx={{
+          p: 1.5,
+          display: 'flex',
+          justifyContent: 'justify-between',
+          alignItems: 'center',
+        }}
+      >
+        <span>workers/{params.name}</span>
+        <Button
+          sx={{ ml: 'auto', textAlign: 'right' }}
+          variant="outlined"
+          color="error"
+          onClick={() => setDeleteOpen(true)}
+        >
+          Delete
+        </Button>
+      </Box>
       <Divider />
       <Box sx={{ p: 1.5 }}>
         <ResourceView
