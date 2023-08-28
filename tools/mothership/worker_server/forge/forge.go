051589caee39e7d3a60dbc3400a7c54f7dde7068
Mustafa Gezen <mustafa@rockylinux.org>
2023-08-28 05:12:22 +0200

    Add support for remote forges

--- tools/mothership/worker_server/forge/forge.go
+++ tools/mothership/worker_server/forge/forge.go
@@ -1,1 +1,22 @@
 package forge
+
+import (
+	"github.com/go-git/go-git/v5/plumbing/transport"
+	"time"
+)
+
+type Authenticator struct {
+	transport.AuthMethod
+
+	AuthorName  string
+	AuthorEmail string
+	// Expires is the time when the token expires.
+	// So it can be used to cache the token.
+	Expires time.Time
+}
+
+type Forge interface {
+	GetAuthenticator() (*Authenticator, error)
+	GetRemote() string
+	EnsureRepositoryExists(auth *Authenticator, repo string) error
+}
